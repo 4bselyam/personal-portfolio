116009886ababc6b7a95c9e62b199306adbf99b5
Akhmed Valiaiev <valiaiev@faber-solartechnik.de>
2025-04-17 15:37:08 +0200

    Hero Section done

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,13 +22,17 @@ const words = [
 	{ text: 'Concepts', imgPath: '/images/concepts.svg' },
 	{ text: 'Designs', imgPath: '/images/designs.svg' },
 	{ text: 'Code', imgPath: '/images/code.svg' },
+	{ text: 'Ideas', imgPath: '/images/ideas.svg' },
+	{ text: 'Concepts', imgPath: '/images/concepts.svg' },
+	{ text: 'Designs', imgPath: '/images/designs.svg' },
+	{ text: 'Code', imgPath: '/images/code.svg' },
 ]
 
 const counterItems = [
-	{ value: 15, suffix: '+', label: 'Years of Experience' },
-	{ value: 200, suffix: '+', label: 'Satisfied Clients' },
-	{ value: 108, suffix: '+', label: 'Completed Projects' },
-	{ value: 90, suffix: '%', label: 'Client Retention Rate' },
+	{ value: 5, suffix: '+', label: 'Years of Experience' },
+	{ value: 15, suffix: '+', label: 'Completed Complex Projects' },
+	{ value: 25, suffix: '+', label: 'Technologies Used' },
+	{ value: 90, suffix: '%', label: 'Client Satisfaction Rate' },
 ]
 
 const logoIconsList = [
