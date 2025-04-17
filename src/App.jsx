bb8a3096c5f8f4bac071d0fe874ed8afacf1959b
Akhmed Valiaiev <valiaiev@faber-solartechnik.de>
2025-04-17 15:16:39 +0200

    Add Tailwind CSS integration, new components and 3D hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import Hero from './sections/Hero'
 
 const App = () => {
 	return (
-		<main>
-			<div>
-				<h1>Welcome to the world of React</h1>
-			</div>
-		</main>
+		<>
+			<Hero />
+		</>
 	)
 }
 
